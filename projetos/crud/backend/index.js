d4bde6e77db265a70714142395bbeb5cc73ff7e9
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2023-03-24 22:12:08 -0300

    Finalização do projeto crud (ReactTS + Axios + MySQL)

--- projetos/crud/backend/index.js
+++ projetos/crud/backend/index.js
@@ -11,10 +11,47 @@ const db = mysql.createPool({
 })
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 
 
-app.get('/api', (req, res)=>{
-    res.send('Oi')
+app.get('/', (req, res)=>{
+    const sql = 'select * from jogos'
+    db.query(sql, (error, result)=>{
+        if(error) console.log(error)
+        else res.send(result)
+    })
+})
+
+app.post('/cadastro', (req, res)=>{
+    const { nome } = req.body
+    const { categoria } = req.body
+    const { ano } = req.body
+
+    const sql = 'insert into jogos (nome, categoria, ano) values (?, ?, ?)'
+    const valores = [nome, categoria, ano]
+    db.query(sql, valores, (error, result)=>{
+        if(error) console.log(error)
+        else res.send(result)
+    })
+})
+
+app.put('/editar', (req, res)=>{
+    const { pk } = req.body
+    const { nome } = req.body
+    const { categoria } = req.body
+    const { ano } = req.body
+
+    const sql = 'update jogos set nome=?, categoria=?, ano=? where nome=?'
+    const valores = [nome, categoria, ano, pk]
+    db.query(sql, valores, (error, result)=>{
+        if(error) console.log(error)
+        else res.send(result)
+    })
+})
+
+app.put('/delete', (req, res)=>{
+    const { pk } = req.body
+    const sql = 'delete from jogos where nome=?'
+    db.query(sql, pk)
 })
 
 app.listen(porta, ()=>{console.log('Servidor rodando')})
